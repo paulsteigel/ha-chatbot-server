5324bd24f5e2ab37f25a1dfb8c32fc1063ef1a04
Đặng Đình Ngọc <paulsteigel@users.noreply.github.com>
2025-10-26 23:12:34 +0700

    Update __init__.py

--- kids-chatbot-server/rootfs/usr/bin/utils/__init__.py
+++ kids-chatbot-server/rootfs/usr/bin/utils/__init__.py
@@ -1,1 +1,6 @@
-"""Utils package for Kids ChatBot Server"""
+"""Utility functions for Kids ChatBot Server"""
+
+from .content_filter import is_safe_content, sanitize_text
+from .response_templates import get_response_template
+
+__all__ = ['is_safe_content', 'sanitize_text', 'get_response_template']
