6e446fabadd50ead093ca910fa28634cebb03580
Đặng Đình Ngọc <paulsteigel@users.noreply.github.com>
2025-10-26 16:29:56 +0700

    Update and rename rootfs/usr/bin/prompts.py to rootfs/usr/bin/utils/__init__.py

---
+++ rootfs/usr/bin/utils/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Utils package for Kids ChatBot Server
+"""
+
+from .content_filter import ContentFilter
+from .response_templates import ResponseTemplates
+
+__all__ = ['ContentFilter', 'ResponseTemplates']
+__version__ = '1.0.0'
