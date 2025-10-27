7e703545e9c361483999332231f46a571b61e0cb
Đặng Đình Ngọc <paulsteigel@users.noreply.github.com>
2025-10-27 09:00:15 +0700

    Update content_filter.py

--- kids-chatbot-server/rootfs/usr/bin/utils/content_filter.py
+++ kids-chatbot-server/rootfs/usr/bin/utils/content_filter.py
@@ -1,3 +1,5 @@
+"""Content filtering utilities"""
+
 def is_safe_content(text):
     """
     Basic content filter for inappropriate content
@@ -6,9 +8,13 @@ def is_safe_content(text):
     
     # List of inappropriate keywords (expand as needed)
     inappropriate_keywords = [
-        'violence', 'weapon', 'drug', 'alcohol',
-        'bạo lực', 'vũ khí', 'ma túy', 'rượu',
-        # Add more keywords as needed
+        # English
+        'violence', 'weapon', 'drug', 'alcohol', 'kill', 'death', 'blood',
+        'sex', 'porn', 'nude', 'hate', 'racist', 'bomb', 'terrorist',
+        
+        # Vietnamese
+        'bạo lực', 'vũ khí', 'ma túy', 'rượu', 'giết', 'chết', 'máu',
+        'sex', 'khiêu dâm', 'khỏa thân', 'ghét', 'phân biệt', 'bom', 'khủng bố',
     ]
     
     text_lower = text.lower()
@@ -18,3 +24,19 @@ def is_safe_content(text):
             return False
     
     return True
+
+
+def sanitize_text(text):
+    """
+    Sanitize text by removing potentially harmful content
+    Returns cleaned text
+    """
+    # Remove excessive whitespace
+    text = ' '.join(text.split())
+    
+    # Remove special characters that might be used for injection
+    dangerous_chars = ['<', '>', '{', '}', '`', '$']
+    for char in dangerous_chars:
+        text = text.replace(char, '')
+    
+    return text.strip()
